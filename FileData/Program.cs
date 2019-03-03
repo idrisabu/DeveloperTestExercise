3eeaff35901025a5ced1ac4c5953a1788c16e328
unknown <idris.abu.ise@gmail.com>
2019-03-03 00:09:52 +0000

    Changes to the Console

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using ThirdPartyTools;
+using Wrappers;
+using InputValidation;
 
 namespace FileData
 {
@@ -9,6 +11,23 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+            FunctionalityArgumentsValidator functionalityArgumentsValidator = new FunctionalityArgumentsValidator();
+            Validation consoleInputValidation = new Validation(functionalityArgumentsValidator);
+            FileDetails fileDetails = new FileDetails();
+            ThirdPartyToolsWrapper thirdPartyToolsWrapper = new ThirdPartyToolsWrapper(fileDetails);
+
+            if (consoleInputValidation.ValidInput(args))
+            {
+                if (functionalityArgumentsValidator.ValidSizeArgument(args[0]))
+                    Console.WriteLine(thirdPartyToolsWrapper.Size(args[1]).ToString());
+                else
+                    Console.WriteLine(thirdPartyToolsWrapper.Version(args[1]).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Invalid input argument(s).");
+            }
+            Console.ReadLine();
         }
     }
 }
